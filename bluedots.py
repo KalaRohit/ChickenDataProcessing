130fe570defbd6c50fc098c66854bd5eb7c014c5
Rohit <kalarohit9@gmail.com>
2022-11-15 14:55:55 -0600

    we now choose all the blue dots

--- bluedots.py
+++ bluedots.py
@@ -2,14 +2,39 @@ import os
 import shutil
 import sys
 import numpy as np
+import cv2
+
+
 
 from PIL import Image
 from typing import List, Tuple
 
-WHITE = [255, 255, 255] 
+WHITE = [255, 255, 255]
+NO_OF_COLORS = 20
+COLORS = {}
 
 #read the csv file for the points. Return as (x,y) 
 def find_points(image: np.array) -> List[Tuple[int]]:
+    lower_blue = np.array([78,48,53])
+    upper_blue = np.array([145,255,255])
+
+    a = np.uint8([[[49, 53, 43]]])
+    b = np.uint8([[[255, 0, 213]]])
+
+    hsv1 = cv2.cvtColor(a, cv2.COLOR_BGR2HSV)
+    hsv2 = cv2.cvtColor(b, cv2.COLOR_BGR2HSV)
+
+    print(hsv1, hsv2)
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    res = cv2.bitwise_not(image, image, mask=mask)
+
+    cv2.imshow('mask', res)
+    cv2.waitKey(0)
+
     return []
 
 def blur_point(image: np.array, point: Tuple[int]) -> np.array:
@@ -31,22 +56,24 @@ def main() -> None:
     except:
         shutil.rmtree('./ModifiedChickenData')
         os.mkdir('./ModifiedChickenData')
+        
     
     for img in images:
-        pic:Image = Image.open(directory+img)
+        pic:Image = cv2.imread(directory+img, cv2.IMREAD_COLOR)
         numpy_pic: np.array = np.asarray(pic)
         
-        for p in find_points(img):
-            blur_point(numpy_pic, p)
+        # for p in find_points(img):
+        #     blur_point(numpy_pic, p)
     
         numpy_pic_array.append(numpy_pic)
+    
+    print(numpy_pic_array[0])
+    find_points(numpy_pic_array[0])
+    # for index, modified_img in enumerate(numpy_pic_array):
+    #     save_img = Image.fromarray(modified_img)
+    #     save_img.save(f'./ModifiedChickenData/{index}.png')
 
-    for index, modified_img in enumerate(numpy_pic_array):
-        save_img = Image.fromarray(modified_img)
-        save_img.save(f'./ModifiedChickenData/{index}.png')
-
-
-
+    
  
 if __name__ == '__main__':
     main()
